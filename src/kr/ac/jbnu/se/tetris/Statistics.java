7db9f16ab77827c27ebaf15934a097e0be1906bc
h-ch22 <ckdwls9460@naver.com>
2019-07-27 12:22:51 +0900

    Now you can choose dark or light theme with block color changed.

---
+++ src/kr/ac/jbnu/se/tetris/Statistics.java
@@ -0,0 +1,14 @@
+package kr.ac.jbnu.se.tetris;
+import java.io.*;
+
+public class Statistics {
+    String name[] = {};
+    String time[] = {};
+    String date[] = {};
+
+    public void Statistics() {
+
+
+    }
+}
+
---
+++ src/kr/ac/jbnu/se/tetris/Tetrominoes.java
@@ -0,0 +1,5 @@
+package kr.ac.jbnu.se.tetris;
+
+public enum Tetrominoes {
+    NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape
+}
